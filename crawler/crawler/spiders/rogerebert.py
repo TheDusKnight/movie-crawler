f7059fe9dbed08384e780457b86d10579508a087
TheDusKnight <860956020@qq.com>
2021-01-28 22:21:00 -0800

    1/28

--- crawler/crawler/spiders/rogerebert.py
+++ crawler/crawler/spiders/rogerebert.py
@@ -1,4 +1,10 @@
 import scrapy
+import uuid
+import datetime
+import re
+from scrapy.spidermiddlewares.httperror import HttpError
+from twisted.internet.error import DNSLookupError
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 # from scrapy.spiders import CrawlSpider, Rule
 from bs4 import BeautifulSoup
 import urllib
@@ -20,16 +26,57 @@ class Rogerebert(scrapy.Spider):
         return
 
     def parse(self, response):
-        print('Current page ' + response.url)
-        nextpage = response.css('a.pagination-next::attr(href)').get()
+        self.logger.info('Index page ' + response.url)
+        # uid = uuid.uuid1()
+        # self.logger.info('id ' + uid.hex)
+
+
+        next_page = response.css('a.pagination-next::attr(href)').get()
         # nextpagetext = response.css('.pagination-next').extract()
 
-        # yield scrapy.Request(nextpage, callback=self.parse)
-        yield response.follow(nextpage, callback=self.parse)
+        content_pages = response.css('h5.review-stack--title a::attr(href)').getall()
+        for page in content_pages:
+            yield response.follow(page, callback=self.parse_page1)
+
+        # yield scrapy.Request(next_page, callback=self.parse)
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
         return
 
+    def parse_page1(self, response):
+        self.logger.info('Content page ' + response.url)
+        uid = uuid.uuid1().hex
+        self.logger.info("id " + uid)
 
-    def parse_next_page(self, response):
-        print('Fetched next page' + response.url)
+        title_year = response.css('h3.cast-and-crew--movie-title::text').get().lower()
+        if not title_year:
+            title = " "
+            year = " "
+        else:
+            title_year = re.sub("\n", "", title_year)
+            title = re.search(r"^[^\(]+", title_year)[0].strip()
+            year = re.search(r"\((\w+)\)", title_year).group(1)
+
+        director = response.css('li[itemprop="director"] span::text').get().lower()
+        casts = response.css('p.cast-and-crew--detail span::text').getall()
+        # TODO: check response.css null error
+        running_time = re.sub("\n", "", response.css('p.cast-and-crew--running-time::text').get())
+        if not director:
+            director = " "
+        if not casts:
+            casts = [" "]
+        if not running_time:
+            running_time = " "
+
+        yield {
+            "id": uid,
+            "url": response.url,
+            "timestamp_crawl": datetime.datetime.now(),
+            "title": title,
+            "release_date": year,
+            "director": director,
+            "cast": casts,
+            "running_time": running_time,
+        }
         return
 
